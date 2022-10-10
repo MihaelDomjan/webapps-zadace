f39b6e0eeb7ab8b8e4d083560963aabcc000d0d2
MihaelDomjan <mihael.domjan83@gmail.com>
2022-10-10 18:32:45 +0200

    Konacno rijesenje 3.zadatka n0.2

--- 01-zadaca/zadatak3.js
+++ 01-zadaca/zadatak3.js
@@ -1,8 +1,7 @@
 //Napiši funkciju koja provjerava nalazi li se dani broj X unutar [100,150000] : Ispis : 53 -> “53 nije unutar [100,150000]”
 
-const BROJ_MIN = 130;
 
-
+const RANDOM_N = 10;
 
 const isNumberInRange = (number) => {
 	return number >= 100 && number <= 150000;
