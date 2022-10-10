1d905984a26f4ca575ed1cfe9daa2edb557e2aa9
MihaelDomjan <mihael.domjan83@gmail.com>
2022-10-10 18:33:49 +0200

    Konacno rijesenje 4.zadatka

--- 01-zadaca/zadatak4.js
+++ 01-zadaca/zadatak4.js
@@ -1,12 +1,16 @@
 //Napiši funkciju koja pretvara broj X u sate i minute - Ispis : 120 -> “Ovo je 2 sata i 0 minuta”
 
-function pretvarac(x) {
-    var x;
-    var sati = (x / 60);
-    var preostaliSati = Math.floor(sati);
-    var minute = (sati - preostaliSati) * 60;
-    var preostaleMinute = Math.round(minute);
-    return x + "=" + preostaliSati + " sati i " + preostaleMinute + " sekunde.";
-    }
-    
-    console.log(pretvarac(20))
+const BROJ_MIN = 130;
+
+const numberToTime = (number) => {
+	const hours = number / 60;
+	const roundedHours = Math.floor(hours);
+	const minutes = (hours - roundedHours) * 60;
+	const roundedMinutes = Math.round(minutes);
+
+	return console.log(
+		`${number} => ${roundedHours} sati i ${roundedMinutes} minuta.`
+	);
+};
+
+numberToTime(BROJ_MIN);
