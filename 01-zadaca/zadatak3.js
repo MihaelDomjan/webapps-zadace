93d0558412afdbbc7cf69743a707425b6915d6b6
MihaelDomjan <mihael.domjan83@gmail.com>
2022-10-10 18:16:09 +0200

    Update zadatak3.js

---
+++ 01-zadaca/zadatak3.js
@@ -0,0 +1,6 @@
+//Napiši funkciju koja provjerava nalazi li se dani broj X unutar [100,150000] : Ispis : 53 -> “53 nije unutar [100,150000]”
+
+let provjera = (broj) => (broj >= 100 && broj <= 150000) 
+
+console.log(43)
+console.log(2343)
